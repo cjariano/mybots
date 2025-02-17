4008795bbfb1bf0831db94fc4ae46d730d05a44e
cjariano <cariano@uvm.edu>
2025-02-17 14:13:36 -0500

    assignment 3

--- simulate.py
+++ simulate.py
@@ -1,11 +1,20 @@
 import pybullet as p
 import time
-physicsClient = p.connect(p.GUI)
-# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+import pybullet_data
 
-for i in range(1000):
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-10, physicsClient)
+
+planeId = p.loadURDF("plane.urdf")
+robotID = p.loadURDF("body.urdf")
+
+p.loadSDF("world.sdf")
+
+for i in range (10000):
     p.stepSimulation()
-    time.sleep(1/60)
+    time.sleep(0.004)
     print(i)
 
 p.disconnect()
