69eb884e12d9d8ac5a3595ab4892fc38630a865b
cjariano <cariano@uvm.edu>
2025-03-16 16:58:35 -0400

    hw9 random search

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -33,6 +34,10 @@ def Generate_Brain():
     pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -3.0)
     pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -4.0)
     pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 2.0)
+    for i in range(0,3):
+        for j in range(3,5):
+            pyrosim.Send_Synapse(sourceNeuronName = i, targetNeuronName = j, weight = np.random.uniform(-1, 1))
+
     pyrosim.End()
 
 
